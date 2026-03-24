f2b4f987a3482aaa2a75c0c7b4513db2a7551f8e
Meghana-0226 <meghanaduttuluri@gmail.com>
2026-03-24 09:24:02 +0530

    UC3-Track Unique Bogie IDs (Set – HashSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,22 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        System.out.println("=== UC2 - Add Passenger Bogies to Train ===");
-        System.out.println("===========================================");
-        List<String> passengerBogies = new ArrayList<>();
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
-        System.out.println("\nAfter Adding Bogies:");
-        System.out.println("Passenger Bogies : " + passengerBogies);
-        passengerBogies.remove("AC Chair");
-        System.out.println("\nAfter Removing 'AC Chair':");
-        System.out.println("Passenger Bogies : " + passengerBogies);
-        System.out.println("\nChecking if 'Sleeper' exists:");
-        System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper"));
-        System.out.println("\nFinal Train Passenger Consist:");
-        System.out.println(passengerBogies);
-        System.out.println("\nUC2 operations completed successfully...");
+        System.out.println("=== UC3 - Track Unique Bogie IDs ===");
+        System.out.println("====================================");
+        Set<String> bogieIds = new HashSet<>();
+        bogieIds.add("BG104");
+        bogieIds.add("BG103");
+        bogieIds.add("BG102");
+        bogieIds.add("BG101");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        System.out.println("\nBogie IDs After Insertion:");
+        System.out.println(bogieIds);
+        System.out.println("\nNote:");
+        System.out.println("Duplicates are automatically ignored by HashSet.");
+        System.out.println("\nUC3 uniqueness validation completed...");
     }
 }
