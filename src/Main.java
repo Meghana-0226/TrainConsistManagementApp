3b920c3e10e20c2ad85efdcf0d35226b2fa1a9c1
Meghana-0226 <meghanaduttuluri@gmail.com>
2026-04-17 10:28:07 +0530

    Safe Cargo Assignment Using try-catch-finally

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,40 @@
 package TrainConsistManagementApp;
- class TrainConsistManagementApp  {
-    public static void main(String[] args) {
-        int[] capacities = {72, 56, 24, 70, 60};
-        System.out.println("UC16 - Manual Sorting using Bubble Sort");
-        System.out.println("=======================================");
-        System.out.println("\nOriginal Capacities:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
-        int n = capacities.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
+class CargoSafetyException extends RuntimeException {
+    public CargoSafetyException(String message) {
+        super(message);
+    }
+}
+class GoodsBogie {
+    String shape;
+    String cargo;
+    GoodsBogie(String shape) {
+        this.shape = shape;
+    }
+    void assignCargo(String cargoType) {
+        try {
+            if (shape.equalsIgnoreCase("Rectangular") &&
+                    cargoType.equalsIgnoreCase("Petroleum")) {
+                throw new CargoSafetyException("Unsafe cargo assignment!");
             }
+            this.cargo = cargoType;
+            System.out.println("Cargo assigned successfully -> " + cargoType);
+        } catch (CargoSafetyException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("Cargo validation completed for " + shape + " bogie");
         }
-        System.out.println("\n\nSorted Capacities (Ascending):");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
-        System.out.println("\n\nUC16 sorting completed...");
+    }
+}
+class TrainConsistManagementApp {   // removed 'public'
+    public static void main(String[] args) {
+        System.out.println("=======================================");
+        System.out.println("UC15 - Safe Cargo Assignment");
+        System.out.println("=======================================\n");
+        GoodsBogie b1 = new GoodsBogie("Cylindrical");
+        b1.assignCargo("Petroleum");
+        System.out.println();
+        GoodsBogie b2 = new GoodsBogie("Rectangular");
+        b2.assignCargo("Petroleum");
+        System.out.println("\nUC15 runtime handling completed...");
     }
 }
